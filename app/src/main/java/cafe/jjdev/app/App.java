8174f136f6e84d34f9dc03e6459699609e93d4e7
Administrator <Administrator@smart20110>
2016-11-25 14:19:47 +0900

    22기 연습예제 첫번째 커밋

---
+++ app/src/main/java/cafe/jjdev/app/App.java
@@ -0,0 +1,15 @@
+package cafe.jjdev.app;
+
+public class App {
+	public static void main(String[] args) {
+	}
+}
+class test{
+	String name;
+	int age;
+	@Override
+	public String toString() {
+		return "test [name=" + name + ", age=" + age + "]";
+	}
+	
+}
